0ca1872b2b512199f217811dba0dab96e2effd71
kennyg37 <k.ganza@alustudent.com>
2024-03-05 21:42:24 +0200

    Responsiveness

--- js/blog.js
+++ js/blog.js
@@ -11,4 +11,10 @@ document.getElementById('scroll-left').addEventListener('click', function() {
         behavior: 'smooth'
       });
   });
-  
+  var sidemenu = document.getElementById("mobile-menu");
+function openmenu(){
+    sidemenu.style.right = "0"
+}
+function closemenu() {
+    sidemenu.style.right = "-350px"
+}
