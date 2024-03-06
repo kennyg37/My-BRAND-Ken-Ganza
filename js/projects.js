498e65af32494547af22001688cc00bb7a21b1a3
kennyg37 <k.ganza@alustudent.com>
2024-03-06 15:15:59 +0200

    responsivity

--- js/projects.js
+++ js/projects.js
@@ -1,6 +1,6 @@
 var sidemenu = document.getElementById("mobile-menu");
 function openmenu(){
-    sidemenu.style.right = "0"
+    sidemenu.style.right = "-100"
 }
 function closemenu() {
     sidemenu.style.right = "-350px"
