842ada0502646afccaa55f44c332ec33b895c00c
kennyg37 <k.ganza@alustudent.com>
2024-04-02 13:58:08 +0200

    Adding tests

---
+++ backend/src/routes/detailsRoute.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import Details from "../models/details";
+
+const router = express.Router()
+
+router.get('details', async(res: Response, req: Request) => {
+    const info = await Details.find()
+    res.send(info)
+})
+
+router.post('/details/edit', async(req: Request, res: Response) => {
+    const {firstName, lastName, phone, socials} = req.params;
+    const info = new Details ({
+        firstName,
+        lastName,
+        phone,
+        socials
+    })
+
+    res.json(info)
+})
--- backend/src/test/authRoutes.test.ts
+++ backend/src/test/authRoutes.test.ts
@@ -1,46 +1,28 @@
-import { expect } from 'chai';
+import chai from 'chai';
 import request from 'supertest'; 
 import express, { Express } from 'express';
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
 import User from '../models/auth';
+import authRoutes from '../routes/authRoutes';
 
-describe('authRoutes', () => {
-  let app: Express;
 
-  before(() => {
-    app = express();
-    app.use('/', require('../routes/auth').default);
-  });
-
-  it('should get user data', async () => {
-    const res = await request(app).get('/data');
-    expect(res.status).to.equal(200);
-  });
-
-  it('should create a new user', async () => {
-  const password = 'password';
-    const hashedPassword = await bcrypt.hash(password, 10)
-    const res = await request(app)
-      .post('/signup')
-      .send({
-        account: 'admin',
-        username: 'test',
-        email: 'test@example.com',
-        password: hashedPassword,
-    });
-    expect(res.status).to.equal(200);
-    expect(res.body).to.have.property('message', 'User created successfully');
-   });
-
-    it('should login a user', async () => {
-        const password = 'password';
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({
-        account: 'admin',
-        username: 'test',
-        password: hashedPassword,
+describe('Auth Routes', () => {
+      let app: Express;
+      before(() => {
+        app = express();
+        app.use('/v1/auth', authRoutes);
+      });
+      it('should get user data', async () => {
+        const res = await request(app).get('/data');
+      });
+      it('should create a new user', async () => {
+        const res = await request(app).post('/signup').send({
+          account: 'admin',
+          username: 'admin',
+          email: 'email@example.com',
+          password: 'password',
+          confirmPassword: 'password',
         });
-       
-    });
+      });
 });
+
