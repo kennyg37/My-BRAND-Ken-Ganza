0217aabea7db5710a54b0e1ba0593e967b2734ee
kennyg37 <k.ganza@alustudent.com>
2024-04-03 11:20:23 +0200

    updating the workflow file to close the server that was started

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,19 +11,25 @@ const PORT = 4000;
 
 mongoose.connect('mongodb://localhost:27017/portfolio',);
 
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 
 app.use('/v1/auth', authRoutes);
 app.use('/v1/blog', blogRoutes);
 app.use('/v1/feedback', contactRoutes);
 app.use('/v1/profile', detailsRoutes);
 
-app.listen(PORT, () => {
+let server: any;
+
+export const startServer = () => {
+  server = app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
-});
+  });
+  return server;
+}
+
+export const closeServer = () => {
+  server.close();
+}
 
 export default app;
---
+++ backend/src/test/server.test.ts
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import app from '../server'; 
+import { startServer, closeServer } from '../server';
+
+describe('App', () => {
+  before(() => {
+    startServer();
+    mongoose.connect('mongodb://localhost:27017/portfolio');
+    mongoose.connection.once('open', () => {
+      console.log('Connected to MongoDB');
+    });
+  });
+
+  after(() => {
+    mongoose.disconnect();
+    closeServer();
+  });
+
+  it('should return a 200 status code for GET /v1/auth', async () => {
+    const res = await request(app).get('/v1/auth');
+  });
+    it('should return a 200 status code for GET /v1/blog', async () => {
+        const res = await request(app).get('/v1/blog');
+    });
+    it('should return a 200 status code for GET /v1/feedback', async () => {
+        const res = await request(app).get('/v1/feedback');
+    });
+});
