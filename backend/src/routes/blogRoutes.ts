19f2fc424535c6e3b6fcd1cec735d1699fe10176
kennyg37 <k.ganza@alustudent.com>
2024-04-24 18:08:09 +0200

    add all files

--- backend/src/routes/blogRoutes.ts
+++ backend/src/routes/blogRoutes.ts
@@ -42,9 +42,129 @@ const upload = multer({
     })
   });
 
+
+
+router.post('/create', verifyToken, upload.single('image'), async (req: Request, res: Response) => {
+    const {title, subtitle, content} = req.body;
+    const image = req.file;
+    
+    if (!image) {
+        const info = new Blog ({
+            title,
+            subtitle,
+            content
+        })
+    await info.save();
+    res.json({message: 'Blog created successfully but has no image'})
+    res.json(info);
+    
+    } else {
+
+        const params = {
+            Bucket: 'kenganzabucket1',
+            Key: `images/${uuidv4()}-${image.originalname}`,
+            Body: image.buffer,
+            ContentType: image.mimetype,
+            ACL: 'public-read'
+          };
+
+        
+
+        const info = new Blog ({
+            title,
+            subtitle,
+            content,
+            image: imageUrl
+        })
+        await info.save();
+        res.json({message: 'Blog created successfully'})
+        res.json(info);
+    }
+});
+
+
+
+
+router.get('/data', async (req: Request, res: Response) => {
+    const info = await Blog.find();
+    res.send(info);
+});
+
+
+router.put('/like/:id', verifyGuestToken, async (req: Request, res: Response) => {
+    const {id} = req.params;
+    const info = await Blog.findByIdAndUpdate({_id: id}, {$inc: {likes: 1}}, {new: true});
+    if (!info) {
+        return res.status(404).json({message: 'Blog not found'})
+    } else {
+        res.json({message: 'Blog liked successfully'})
+    }
+});
+
+
+router.put('/comment/:id', verifyGuestToken, async (req: Request, res: Response) => {
+    const {id} = req.params;
+    const {comment} = req.body;
+    const info = await Blog.findByIdAndUpdate({_id: id}, {$push: {comments: comment}, $inc: {commentsCount: 1}}, {new: true});
+
+    if (!info) {
+        return res.status(404).json({message: 'Blog not found'})
+    } else {
+        res.json({message: 'Comment added successfully', comments: info['comments']})
+    }
+});
+
+
+router.put('/update/:id', verifyToken, async (req: Request, res: Response) => {
+    const {id} = req.params;
+    const {title, subtitle, content} = req.body;
+    const info = await Blog.findByIdAndUpdate({_id: id}, {title, subtitle, content}, {new: true});
+
+    if (!info) {
+        return res.status(404).json({message: 'Blog not found'})
+    } else {
+        res.json({message: 'Blog updated successfully'})
+    }
+});
+
+
+
+router.delete('/delete/:id', verifyToken, async (req: Request, res: Response) => {
+    const {id} = req.params;
+    const info = await Blog.findByIdAndDelete({_id: id});
+    if (!info) {
+        return res.status(404).json({message: 'Blog not found'})
+    } else {
+        res.json({message: 'Blog deleted successfully'})
+    }
+});
+
+
+router.delete('/delete/comment/:id', verifyGuestToken, async (req: Request, res: Response) => {
+    const {id} = req.params;
+    const {comment} = req.body;
+    const info = await Blog.findByIdAndUpdate({_id: id}, {$pull: {comments: comment}, $inc: {commentsCount: -1}}, {new: true});
+
+    if (!info) {
+        return res.status(404).json({message: 'Blog not found'})
+    } else {
+        res.json({message: 'Comment deleted successfully'})
+    }
+});
+
+
+router.delete('/delete/like/:id', verifyGuestToken, async (req: Request, res: Response) => {
+    const {id} = req.params;
+    const info = await Blog.findByIdAndUpdate({_id: id}, {$inc: {likes: -1}}, {new: true});
+    if (!info) {
+        return res.status(404).json({message: 'Blog not found'})
+    } else {
+        res.json({message: 'Blog unliked successfully'})
+    }
+});
 /**
  * @swagger
- * https://my-brand-ken-ganza-1.onrender.com/v1/blog/create:
+ * /v1/blog/create:
  *   post:
  *     summary: Create a new blog post
  *     description: Creates a new blog post with an optional image upload.
@@ -90,261 +210,48 @@ const upload = multer({
  *         description: Unauthorized
  */
 
-router.post('/create', verifyToken, upload.single('image'), async (req: Request, res: Response) => {
-    const {title, subtitle, content} = req.body;
-    const image = req.file;
-    
-    if (!image) {
-        const info = new Blog ({
-            title,
-            subtitle,
-            content
-        })
-    await info.save();
-    res.json({message: 'Blog created successfully but has no image'})
-    res.json(info);
-    
-    } else {
-
-        const params = {
-            Bucket: 'kenganzabucket1',
-            Key: `images/${uuidv4()}-${image.originalname}`,
-            Body: image.buffer,
-            ContentType: image.mimetype,
-            ACL: 'public-read'
-          };
-
-        
-
-        const info = new Blog ({
-            title,
-            subtitle,
-            content,
-            image: imageUrl
-        })
-        await info.save();
-        res.json({message: 'Blog created successfully'})
-        res.json(info);
-    }
-});
-
-
 /**
  * @swagger
  * /v1/blog/data:
  *   get:
- *     summary: Retrieve all blogs.
+ *     summary: Get all blog posts
+ *     description: Retrieves all blog posts.
+ *     tags:
+ *       - Blog
  *     responses:
- *       '200':
- *         description: A list of blogs.
- */
-
-router.get('/data', async (req: Request, res: Response) => {
-    const info = await Blog.find();
-    res.send(info);
-});
-
-
-
-
-
-/**
- * @swagger
- * /v1/blog/like/{id}:
- *   put:
- *     summary: Like a blog.
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: ID of blog to like.
+ *       200:
+ *         description: A list of blog posts
  *         schema:
- *           type: string
- *     responses:
- *       '200':
- *         description: Blog liked successfully.
- */
-
-router.put('/like/:id', verifyGuestToken, async (req: Request, res: Response) => {
-    const {id} = req.params;
-    const info = await Blog.findByIdAndUpdate({_id: id}, {$inc: {likes: 1}}, {new: true});
-    if (!info) {
-        return res.status(404).json({message: 'Blog not found'})
-    } else {
-        res.json({message: 'Blog liked successfully'})
-    }
-});
-
-/**
- * @swagger
- * /v1/blog/comment/{id}:
- *   put:
- *     summary: Comment on a blog.
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: ID of blog to comment on.
- *         schema:
- *           type: string
- *     requestBody:
- *       required: true
+ *           type: array
+ *           items:
+ *             $ref: '#/definitions/BlogPost'
+ * definitions:
+ *   BlogPost:
+ *     type: object
+ *     properties:
+ *       _id:
+ *         type: string
+ *         description: The unique identifier of the blog post
+ *       title:
+ *         type: string
+ *         description: The title of the blog post
+ *       subtitle:
+ *         type: string
+ *         description: The subtitle of the blog post
  *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               comment:
- *                 type: string
- *     responses:
- *       '200':
- *         description: Comment added successfully.
+ *         type: string
+ *         description: The content of the blog post
+ *       image:
+ *         type: string
+ *         description: The URL of the blog post's image
+ *       likes:
+ *         type: number
+ *         description: The number of likes the blog post has received
+ *       commentsCount:
+ *         type: number
+ *         description: The number of comments the blog post has received
  */
 
-router.put('/comment/:id', verifyGuestToken, async (req: Request, res: Response) => {
-    const {id} = req.params;
-    const {comment} = req.body;
-    const info = await Blog.findByIdAndUpdate({_id: id}, {$push: {comments: comment}, $inc: {commentsCount: 1}}, {new: true});
 
-    if (!info) {
-        return res.status(404).json({message: 'Blog not found'})
-    } else {
-        res.json({message: 'Comment added successfully', comments: info['comments']})
-    }
-});
-
-/**
- * @swagger
- * /v1/blog/update/{id}:
- *   put:
- *     summary: Update a blog by ID.
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               title:
- *                 type: string
- *               subtitle:
- *                 type: string
- *               content:
- *                 type: string
- *     responses:
- *       '200':
- *         description: Blog updated successfully.
- *       '404':
- *         description: Blog not found.
- */
-
-router.put('/update/:id', verifyToken, async (req: Request, res: Response) => {
-    const {id} = req.params;
-    const {title, subtitle, content} = req.body;
-    const info = await Blog.findByIdAndUpdate({_id: id}, {title, subtitle, content}, {new: true});
-
-    if (!info) {
-        return res.status(404).json({message: 'Blog not found'})
-    } else {
-        res.json({message: 'Blog updated successfully'})
-    }
-});
-
-/**
- * @swagger
- * /v1/blog/delete/{id}:
- *   delete:
- *     summary: Delete a blog by ID.
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       '200':
- *         description: Blog deleted successfully.
- *       '404':
- *         description: Blog not found.
- */
-
-router.delete('/delete/:id', verifyToken, async (req: Request, res: Response) => {
-    const {id} = req.params;
-    const info = await Blog.findByIdAndDelete({_id: id});
-    if (!info) {
-        return res.status(404).json({message: 'Blog not found'})
-    } else {
-        res.json({message: 'Blog deleted successfully'})
-    }
-});
-
-/**
- * @swagger
- * /v1/blog/delete/comment/{id}:
- *   delete:
- *     summary: Delete a comment from a blog by ID.
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *       - in: query
- *         name: comment
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       '200':
- *         description: Comment deleted successfully.
- *       '404':
- *         description: Blog not found.
- */
-
-router.delete('/delete/comment/:id', verifyGuestToken, async (req: Request, res: Response) => {
-    const {id} = req.params;
-    const {comment} = req.body;
-    const info = await Blog.findByIdAndUpdate({_id: id}, {$pull: {comments: comment}, $inc: {commentsCount: -1}}, {new: true});
-
-    if (!info) {
-        return res.status(404).json({message: 'Blog not found'})
-    } else {
-        res.json({message: 'Comment deleted successfully'})
-    }
-});
-
-/**
- * @swagger
- * /v1/blog/delete/like/{id}:
- *   delete:
- *     summary: Remove a like from a blog by ID.
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       '200':
- *         description: Blog unliked successfully.
- *       '404':
- *         description: Blog not found.
- */
-
-router.delete('/delete/like/:id', verifyGuestToken, async (req: Request, res: Response) => {
-    const {id} = req.params;
-    const info = await Blog.findByIdAndUpdate({_id: id}, {$inc: {likes: -1}}, {new: true});
-    if (!info) {
-        return res.status(404).json({message: 'Blog not found'})
-    } else {
-        res.json({message: 'Blog unliked successfully'})
-    }
-});
 
 export default router;
